e1fdeb593b7edca79afb1959dd5aed58c7a715eb
aayushma pokhrel <pokharelaayushma02@gmail.com>
2025-12-10 11:28:37 +0545

    chore: add Swagger/OpenAPI documentation with drf-yasg

--- core/settings.py
+++ core/settings.py
@@ -45,7 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'authapp',
+    'drf_yasg'
+
 ]
 
 MIDDLEWARE = [
